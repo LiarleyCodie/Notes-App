feb16bcf8506dbece5045d472ebf6b7f2ecc3652
LiarleyCodie <liarley.personal@gmail.com>
2022-06-29 13:31:14 -0300

    Icone e texto no container de notas para quando nao tiver notas

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
-const dropdownMenu = document.getElementById("dropdownMenu");
-const notePrefs = document.getElementById("notePrefs");
-const deleteOption = document.getElementById("deleteOption");
+const notesContainer = document.getElementById("notesContainer");
 
-notePrefs.addEventListener("mouseover", function () {
-  dropdownMenu.style.visibility = "visible";
-});
 
-window.addEventListener("click", () => {
-  dropdownMenu.style.visibility = "hidden";
-});
 
-deleteOption.addEventListener("click", () => console.log(deleteOption))
+// const dropdownMenu = document.getElementById("dropdownMenu");
+// const notePrefs = document.getElementById("notePrefs");
+// const deleteOption = document.getElementById("deleteOption");
+
+// notePrefs.addEventListener("mouseover", function () {
+//   dropdownMenu.style.visibility = "visible";
+// });
+
+// window.addEventListener("click", () => {
+//   dropdownMenu.style.visibility = "hidden";
+// });
+
+// deleteOption.addEventListener("click", () => console.log(deleteOption))
